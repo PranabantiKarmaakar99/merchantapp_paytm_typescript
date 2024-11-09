05235cfb37bd29e33975d41e099ed84c56a52872
PK <aankkhiz@gmail.com>
2024-11-09 13:37:01 +0530

    initial commit

---
+++ src/index.ts
@@ -0,0 +1,7 @@
+import express from 'express';
+import { userRouter } from './router/user';
+
+
+const app = express();
+
+app.use('/api/v1/user',userRouter);
---
+++ src/router/user.ts
@@ -0,0 +1,12 @@
+import {Router} from 'express';
+
+
+export const userRouter = Router();
+
+userRouter.post('/signin',(req,res)=>{
+
+})
+
+userRouter.post('/signup',(req,res)=>{
+    
+})
